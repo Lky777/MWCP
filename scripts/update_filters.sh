bd5a5113f4e7f3712b66eac40090930afc3fce62
Lky777 <73606056+Lky777@users.noreply.github.com>
2025-05-03 05:55:02 +0800

    Add files via upload

--- scripts/update_filters.sh
+++ scripts/update_filters.sh
@@ -50,9 +50,23 @@ sed -i '
   /\/\\/d
 ' rules/combined_rules.txt
 rm -f rules/Easyprivacy_a.txt
+
 #简单去重
-echo ">> 正在执行去重脚本..."
-bash scripts/dedup_rules.sh
+tr 'A-Z' 'a-z' < rules/combined_rules.txt > rules/combined_rules.tmp && mv rules/combined_rules.tmp rules/combined_rules.txt
+rm -f rules/combined_rules.tmp
+# index() 检查是否包含特定字符串（无需转义）
+awk '
+    !(
+        (!/@/ && !/~/)
+        && (
+            index($0, ".gif?") ||
+            index($0, "/ad/")  ||
+            index($0, "/ads/")
+        )
+    )
+' rules/combined_rules.txt > rules/combined_rules.tmp && mv rules/combined_rules.tmp rules/combined_rules.txt
+rm -f rules/combined_rules.tmp
+echo -e ".gif?\n/ad/\n/ads/" >> rules/combined_rules.txt
 
 # 7. 生成最终规则文件
 {
