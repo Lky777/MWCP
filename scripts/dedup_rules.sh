6d3715cca9fb1f1d38db0653e775ff0d357e5739
Lky777 <73606056+Lky777@users.noreply.github.com>
2025-05-03 05:15:39 +0800

    Add files via upload

--- scripts/dedup_rules.sh
+++ scripts/dedup_rules.sh
@@ -2,9 +2,6 @@
 
 tr 'A-Z' 'a-z' < rules/combined_rules.txt > rules/combined_rules.tmp && mv rules/combined_rules.tmp rules/combined_rules.txt
 
-sed -i '/\.gif?.*/!b; /[@~]/d' rules/combined_rules.txt
-sed -i '/\/ad\//!b; /[@~]/d' rules/combined_rules.txt
-sed -i '/\/ads\//!b; /[@~]/d' rules/combined_rules.txt
-
+awk '(/\.gif\?/ || /\/ads\//) && !/[@~]/ {next} {print}' rules/combined_rules.txt > rules/combined_rules.tmp && mv rules/combined_rules.tmp rules/combined_rules.txt
 rm -f rules/combined_rules.tmp
 echo -e ".gif?\n/ad/\n/ads/" >> rules/combined_rules.txt
