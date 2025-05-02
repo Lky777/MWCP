18e14a258045474af5fda4d0d19f863188fd8899
Lky777 <73606056+Lky777@users.noreply.github.com>
2025-05-03 05:36:28 +0800

    Add files via upload

--- scripts/dedup_rules.sh
+++ scripts/dedup_rules.sh
@@ -1,7 +1,13 @@
 #!/usr/bin/env bash
 
 tr 'A-Z' 'a-z' < rules/combined_rules.txt > rules/combined_rules.tmp && mv rules/combined_rules.tmp rules/combined_rules.txt
+# index() 检查是否包含特定字符串（无需转义）
+awk '!(
+    (index($0, ".gif?") || 
+     index($0, "/ad/") || 
+     index($0, "/ads/")) 
+    && !/@/ 
+    && !/~/
+)' combined_rules.txt > tmp && mv tmp combined_rules.txt
 
-awk '(/\.gif\?/ || /\/ads\//) && !/[@~]/ {next} {print}' rules/combined_rules.txt > rules/combined_rules.tmp && mv rules/combined_rules.tmp rules/combined_rules.txt
-rm -f rules/combined_rules.tmp
 echo -e ".gif?\n/ad/\n/ads/" >> rules/combined_rules.txt
