5724fa0a5774291e4ffdd687f274ae997288f311
Lky777 <73606056+Lky777@users.noreply.github.com>
2025-06-23 06:06:45 +0800

    Add files via upload

--- scripts/matrix_fuel.sh
+++ scripts/matrix_fuel.sh
@@ -1,14 +1,15 @@
 #!/usr/bin/env bash
 set -euo pipefail
-
-wget -P rules/ https://raw.githubusercontent.com/Lky777/MWCP/main/rules/falseblock.txt
+#
 wget -P rules/ https://raw.githubusercontent.com/Lky777/MWCP/main/rules/supple.txt
 wget -P rules/ https://raw.githubusercontent.com/Lky777/MWCP/main/rules/asservs.txt
-# 1. del false positives
+wget -P rules/ https://raw.githubusercontent.com/Lky777/MWCP/main/rules/falseblock.txt
+#
+cat rules/asservs.txt >> rules/matrix.txt
+cat rules/supple.txt >> rules/matrix.txt
 grep -ivFf rules/falseblock.txt rules/matrix.txt > rules/matrix_small.txt
-cat rules/supple.txt >> rules/matrix_small.txt
-cat rules/asservs.txt >> rules/matrix_small.txt
-rm -f rules/falseblock.txt
+#
 rm -f rules/supple.txt
 rm -f rules/asservs.txt
 rm -f rules/matrix.txt
+rm -f rules/falseblock.txt
