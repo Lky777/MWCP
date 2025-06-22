181b866737618dcc226efb00b5cd202579a1317c
Lky777 <73606056+Lky777@users.noreply.github.com>
2025-06-22 22:08:22 +0800

    Add files via upload

--- scripts/surfing_prepare.sh
+++ scripts/surfing_prepare.sh
@@ -4,10 +4,15 @@ set -o nounset
 
 cleanup() {
     echo "Cleaning up temporary files..."
+    rm -f rules/test1.txt
     rm -f rules/adblock.sorted
-    rm -f rules/test1.sorted
-    rm -f rules/test1.tmp
+    rm -f rules/sand_wash.tmp
+    rm -f rules/sand_wash.txt
+    rm -f rules/sand_wash.sorted
     rm -f rules/adblock.txt.bak
+    rm -f rules/pre_surfing.txt
+    rm -f rules/surfing.txt
+    rm -f rules/asservs.txt
     echo "Cleanup completed."
 }
 
@@ -25,8 +30,8 @@ fi|fj|fr|hk|hr|ie|il|in|ir|int|it|jp|ke|kr|ma|mo|mx|my|\
 ng|nl|no|nz|ph|pl|pt|ru|sa|se|sg|sk|th|tw|uk|us|vn|za)([\/[:space:]]|$)"
 
 if [[ -f "rules/test1.txt" ]]; then
-    grep -vE "$DOMAINS" "rules/test1.txt" > "rules/test1.tmp"
-    mv "rules/test1.tmp" "rules/test1.txt"
+    grep -vE "$DOMAINS" "rules/test1.txt" > "rules/sand_wash.tmp"
+    mv "rules/sand_wash.tmp" "rules/sand_wash.txt"
     echo "Finished. Removed other regional websites."
 else
     echo "test1.txt not found" >&2
@@ -44,19 +49,24 @@ else
 fi
 
 sort -u rules/adblock.txt -o rules/adblock.sorted
-sort -u rules/test1.txt -o rules/test1.sorted
+sort -u rules/sand_wash.txt -o rules/sand_wash.sorted
 
 # figure out regular sites--surfing.txt
-comm -23 rules/test1.sorted rules/adblock.sorted > rules/test1.filtered
-mv rules/test1.filtered rules/surfing.txt
+comm -23 rules/sand_wash.sorted rules/adblock.sorted > rules/pre_surfing.txt
+touch "rules/surfing.txt"
+mv rules/pre_surfing.txt rules/surfing.txt
+
+# figure out adservers from sand_wash
+comm -23 rules/sand_wash.sorted rules/surfing.txt > rules/asservs.tmp
+mv rules/asservs.tmp rules/asservs.txt
 
 # Git
 git config --global user.name "GitHub Actions"
 git config --global user.email "actions@github.com"
 
 #
-if ! git diff --quiet -- rules/surfing.txt; then
-    git add rules/surfing.txt
+if ! git diff --quiet -- rules/surfing.txt rules/asservs.txt; then
+    git add rules/surfing.txt /rules/asservs.txt
     git commit -m "Auto-update: $(date -u +'%Y-%m-%d %H:%M UTC')"
     
     for i in {1..3}; do
