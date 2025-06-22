ae31856a68edd9e21b3103a65d4a6a9596430612
Lky777 <73606056+Lky777@users.noreply.github.com>
2025-06-22 08:00:08 +0800

    Add files via upload

--- scripts/update_filters.sh
+++ scripts/update_filters.sh
@@ -75,7 +75,7 @@ rm -f rules/easylist_ads.txt
 rm -f rules/easyprivacy_a.txt
 
 # 7.rules compression
-chmod +x /scripts/rule_compression.sh
+chmod +x ./scripts/rule_compression.sh
 ./scripts/rule_compression.sh rules/rule_pre_del.txt rules/combined_rules.txt
 cat rules/rule_pre_add.txt >> rules/combined_rules.txt
 rm -f rules/rule_pre_del.txt
