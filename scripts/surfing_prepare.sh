1f71bbcd287accf09b47c1dcb74fa67615a0a5df
Lky777 <73606056+Lky777@users.noreply.github.com>
2025-06-22 22:36:37 +0800

    Add files via upload

--- scripts/surfing_prepare.sh
+++ scripts/surfing_prepare.sh
@@ -2,22 +2,6 @@
 set -eo pipefail
 set -o nounset
 
-cleanup() {
-    echo "Cleaning up temporary files..."
-    rm -f rules/test1.txt
-    rm -f rules/adblock.sorted
-    rm -f rules/sand_wash.tmp
-    rm -f rules/sand_wash.txt
-    rm -f rules/sand_wash.sorted
-    rm -f rules/adblock.txt.bak
-    rm -f rules/pre_surfing.txt
-    rm -f rules/surfing.txt
-    rm -f rules/asservs.txt
-    echo "Cleanup completed."
-}
-
-trap cleanup EXIT
-
 rm -rf rules/
 mkdir -p rules/
 
@@ -38,34 +22,12 @@ else
     exit 1
 fi
 
-# get bigdata of adservers
-wget -P rules/ https://raw.githubusercontent.com/badmojr/1Hosts/master/Lite/adblock.txt
-
-if [[ -f "rules/adblock.txt" ]]; then
-    sed -i.bak 's/^||//;s/\^$//' rules/adblock.txt
-else
-    echo "adblock.txt not found" >&2
-    exit 1
-fi
-
-# figure out regular sites--surfing.txt
-sort -u rules/adblock.txt -o rules/adblock.sorted
-sort -u rules/sand_wash.txt -o rules/sand_wash.sorted
-comm -23 rules/sand_wash.sorted rules/adblock.sorted > rules/pre_surfing.txt
-touch "rules/surfing.txt"
-mv rules/pre_surfing.txt rules/surfing.txt
-
-# figure out adservers from sand_wash
-sort -u rules/surfing.txt -o rules/surfing2.sorted
-comm -23 rules/sand_wash.sorted rules/surfing2.sorted > rules/asservs.filter
-mv rules/asservs.filter rules/asservs.txt
-
 # Git
 git config --global user.name "GitHub Actions"
 git config --global user.email "actions@github.com"
 
-if ! git diff --quiet -- rules/surfing.txt rules/asservs.txt; then
-    git add rules/surfing.txt rules/asservs.txt
+if ! git diff --quiet -- rules/sand_wash.txt; then
+    git add rules/sand_wash.txt
     git commit -m "Auto-update: $(date -u +'%Y-%m-%d %H:%M UTC')"
 
     for i in {1..3}; do
