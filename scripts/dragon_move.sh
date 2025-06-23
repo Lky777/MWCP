6dfb8cb34aa065c7d5545c8703404c58d70c7704
Lky777 <73606056+Lky777@users.noreply.github.com>
2025-06-24 05:43:21 +0800

    Add files via upload

--- scripts/dragon_move.sh
+++ scripts/dragon_move.sh
@@ -1,11 +1,10 @@
 #!/usr/bin/env bash
 set -euo pipefail
 
-sort rules/dragon_add.txt
-sort rules/dragon_del.txt
 cat rules/dragon_add.txt >> rules/dragon.txt
 sort rules/dragon.txt
-comm -23 rules/dragon.txt rules/dragon_del.txt > rules/dragon.txt
+awk 'NR==FNR{a[$0];next} !($0 in a)' rules/dragon_del.txt rules/dragon.txt > rules/dragon.tmp && mv rules/dragon.tmp rules/dragon.txt
+sort rules/dragon.txt
 
 # 2. Git push
 git config --global user.name "GitHub Actions"
