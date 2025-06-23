e76c759542ef1a5dd4e5982fdf0578b1091089b4
Lky777 <73606056+Lky777@users.noreply.github.com>
2025-06-24 07:22:41 +0800

    Add files via upload

--- scripts/dragon_move.sh
+++ scripts/dragon_move.sh
@@ -2,7 +2,8 @@
 set -euo pipefail
 
 cat rules/dragon_add.txt >> rules/dragon.txt
-awk 'NR==FNR{a[$0];next} !($0 in a)' rules/dragon_del.txt rules/dragon.txt > rules/dragon.tmp && mv rules/dragon.tmp rules/dragon.txt
+grep -vFf rules/dragon_del.txt rules/dragon.txt > rules/dragon.tmp && \
+mv rules/dragon.tmp rules/dragon.txt
 sort -u -o rules/dragon.txt rules/dragon.txt
 
 # Git push
