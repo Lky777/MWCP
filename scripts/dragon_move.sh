5b01ccf94c528117ff7593e086887db937ebe5ec
Lky777 <73606056+Lky777@users.noreply.github.com>
2025-06-24 08:49:17 +0800

    Add files via upload

--- scripts/dragon_move.sh
+++ scripts/dragon_move.sh
@@ -2,6 +2,6 @@
 set -euo pipefail
 
 cat rules/dragon_add.txt >> rules/dragon.txt
-grep -vFf rules/dragon_del.txt rules/dragon.txt > tmp.txt && \
+grep -vFf <(grep -v '^$' rules/dragon_del.txt) rules/dragon.txt > tmp.txt && \
 mv tmp.txt rules/dragon.txt
 LC_ALL=C sort -u -o rules/dragon.txt rules/dragon.txt
