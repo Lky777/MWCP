8f16b35119ff79189cb575f4b03b657744ae78cb
Lky777 <73606056+Lky777@users.noreply.github.com>
2025-06-24 10:17:48 +0800

    Delete scripts/dragon_move.sh

--- scripts/dragon_move.sh
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env bash
-set -euo pipefail
-
-cat rules/dragon_add.txt >> rules/dragon.txt
-LC_ALL=C sort -u -o rules/dragon.txt rules/dragon.txt
-grep -v -F -x -f rules/dragon_del.txt rules/dragon.txt > rules/dragon_new.txt && \
-mv rules/dragon_new.txt rules/dragon.txt
