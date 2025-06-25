93a373faa0b3acaeac704e969cb1b3496bfc1888
Lky777 <73606056+Lky777@users.noreply.github.com>
2025-06-25 13:47:01 +0800

    Update matrix_launch.sh

--- scripts/matrix_launch.sh
+++ scripts/matrix_launch.sh
@@ -4,10 +4,10 @@ set -euo pipefail
 # 1. general final filterlist
 {
   printf '%s\n'     "[Adblock Plus 2.0]"     "! Title: MobiListChina"     "! Description: blocker for Chinese mobile site"     "! Version: $(date +%Y%m%d%H%M)"     "! Last modified: $(date -u +"%d %b %Y %H:%M UTC")"     "! Expires: 1 day"     "! Homepage: https://github.com/Lky777/MWCP/"     "! ---------------------------------"
-  grep -v -e '^!' -e '^$' -e '^[[:space:]]*$' rules/matrix_small.txt | sort -u
+  grep -v -e '^!' -e '^$' -e '^[[:space:]]*$' rules/matrix.txt | sort -u
 } > rules/MobiListChina.txt
 rule_count=$(grep -v -c -e '^!' rules/MobiListChina.txt)
-rm -f rules/matrix_small.txt
+rm -f rules/matrix.txt
 
 # 2. Git push
 git config --global user.name "GitHub Actions"
