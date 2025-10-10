388a1d4be465ab48dda2d8a0f50392c189345f19
Lky777 <73606056+Lky777@users.noreply.github.com>
2025-10-10 17:34:35 +0800

    Add files via upload

--- scripts/DomainExclusion.sh
+++ scripts/DomainExclusion.sh
@@ -2,6 +2,11 @@
 set -e
 
 input_file="$(pwd)/source/top100k.txt"
-temp_file=$(mktemp)
-awk '/\.(com|cn|net|top|xyz)$/' "$input_file" > "$temp_file" && \
-mv "$temp_file" "$input_file"
+temp_file1=$(mktemp)
+temp_file2=$(mktemp)
+
+awk '/\.(com|cn|net|top|xyz)$/' "$input_file" > "$temp_file1"
+awk '!/\.(gov|edu|mil)\.cn$/' "$temp_file1" > "$temp_file2"
+
+mv "$temp_file2" "$input_file"
+rm "$temp_file1"
