a18aed82a47e4fd284ce533c6cf295e4a8a72538
Lky777 <73606056+Lky777@users.noreply.github.com>
2026-03-13 02:30:17 +0800

    Add files via upload

--- scripts/matrix_launch.sh
+++ scripts/matrix_launch.sh
@@ -3,7 +3,7 @@ set -euo pipefail
 
 # 1. general final filterlist
 {
-  printf '%s\n'     "[Adblock Plus 2.0]"     "! Title: MobiList"     "! Description: blocker for mobile internet"     "! Version: $(date +%Y%m%d%H%M)"     "! Last modified: $(date -u +"%d %b %Y %H:%M UTC")"     "! Expires: 1 day"     "! Homepage: https://github.com/Lky777/MWCP/"     "! ---------------------------------"
+  printf '%s\n'     "[Adblock Plus 2.0]"     "! Title: MobiList"     "! Description: Disruptive Online Content List"     "! Version: $(date +%Y%m%d%H%M)"     "! Last modified: $(date -u +"%d %b %Y %H:%M UTC")"     "! Expires: 1 day"     "! Homepage: https://github.com/Lky777/MWCP/"     "! ---------------------------------"
   grep -v -e '^!' -e '^$' -e '^[[:space:]]*$' rules/matrix.txt | sort -u
 } > rules/MobiList.txt
 rule_count=$(grep -v -c -e '^!' rules/MobiList.txt)
